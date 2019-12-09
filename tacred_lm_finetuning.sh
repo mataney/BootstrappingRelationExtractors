9a0fd4a59e882db20cfdd058100f2d973041a386
mataney <mataneyal1@gmail.com>
2019-12-09 13:58:48 +0200

    create datafiles script

--- tacred_lm_finetuning.sh
+++ tacred_lm_finetuning.sh
@@ -18,11 +18,11 @@ if [ $local_test -eq 0 ];then
         --model_type=gpt2 \
         --model_name_or_path=gpt2 \
         --do_train \
-        --train_data_file=$HOME/matan/dev/datasets/tacred/data/json/train.json \
+        --train_data_file=$HOME/matan/dev/datasets/tacred/data/json/dev.json \
         --block_size=512 \
         --per_gpu_train_batch_size=2 \
         --num_train_epochs=2 \
-        --save_steps=500
+        --save_steps=1000
         # --fp16
 else
     python run_lm_finetuning.py \
@@ -30,10 +30,10 @@ else
         --model_type=gpt2 \
         --model_name_or_path=gpt2-medium \
         --do_train \
-        --train_data_file=$HOME/matan/dev/datasets/tacred/data/json/train.json \
+        --train_data_file=$HOME/matan/dev/datasets/tacred/data/json/dev.json \
         --block_size=512 \
         --per_gpu_train_batch_size=2 \
-        --num_train_epochs=2 \
-        --save_steps=500
+        --num_train_epochs=10 \
+        --save_steps=1000
         # --fp16
 fi
