c98961a180b8a0013627a20a33ca8d6f5b930bce
mataney <mataneyal1@gmail.com>
2020-02-27 11:03:15 +0200

    time it

--- run_classification.sh
+++ run_classification.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This should be in the home directory
 
+start=`date +%s`
 # activate some conda environment
 source activate hugging_face
 
@@ -64,21 +65,6 @@ confidence_threshold_on_train_eval=$(jq -r ".best_confidence" "$output_dir/full_
 
 python -m classification.evaluation.evaluation --gold_dir data/DocRED --gold_file dev.json --relation_name $relation_name --pred_file "$output_dir/full_dev_eval_results.json" --confidence_threshold $confidence_threshold_on_train_eval --output_file "$output_dir/full_dev_eval_scores.json"
 
-jq -n --slurpfile train_eval_scores "$output_dir/full_train_eval_scores.json" \
-  --slurpfile dev_eval_scores "$output_dir/full_dev_eval_scores.json" \
-  --slurpfile num_examples "$OTO_BACKUP/num_examples.json" \
-  '{
-    dev_F1:$dev_eval_scores[0].F1,
-    dev_precision:$dev_eval_scores[0].precision,
-    dev_recall:$dev_eval_scores[0].recall,
-    confidence:$dev_eval_scores[0].best_confidence,
-    train_F1:$train_eval_scores[0].F1,
-    train_precision:$train_eval_scores[0].precision,
-    train_recall:$train_eval_scores[0].recall,
-    num_examples:$num_examples,
-    }' \
-  > "$OTO_OUTPUT"
-
 jq -n --slurpfile train_eval_content "$output_dir/full_train_eval_results.json" \
   --slurpfile dev_eval_content "$output_dir/full_dev_eval_results.json" \
   --slurpfile train_eval_scores "$output_dir/full_train_eval_scores.json" \
@@ -97,3 +83,25 @@ jq -n --slurpfile train_eval_content "$output_dir/full_train_eval_results.json" 
     full_train_eval_results:$train_eval_content
     }' \
   > "$OTO_BACKUP/full_results.json"
+
+
+end=`date +%s`
+secs=$((end-start))
+time="$(($secs/3600))h$(($secs%3600/60))m$(($secs%60))s"
+
+jq -n --arg time $time \
+  --slurpfile train_eval_scores "$output_dir/full_train_eval_scores.json" \
+  --slurpfile dev_eval_scores "$output_dir/full_dev_eval_scores.json" \
+  --slurpfile num_examples "$OTO_BACKUP/num_examples.json" \
+  '{
+    dev_F1:$dev_eval_scores[0].F1,
+    dev_precision:$dev_eval_scores[0].precision,
+    dev_recall:$dev_eval_scores[0].recall,
+    confidence:$dev_eval_scores[0].best_confidence,
+    train_F1:$train_eval_scores[0].F1,
+    train_precision:$train_eval_scores[0].precision,
+    train_recall:$train_eval_scores[0].recall,
+    num_examples:$num_examples,
+    time:$time
+    }' \
+  > "$OTO_OUTPUT"
